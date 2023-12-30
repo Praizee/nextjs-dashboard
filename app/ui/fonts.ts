47666bf432d70b4e029c09e02e45707a83c5644e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-12-31 00:32:28 +0100

    feat: import next fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lusitana, Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
